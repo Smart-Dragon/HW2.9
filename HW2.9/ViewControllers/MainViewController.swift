927a8d31408a1027843fabd492e7775eebb2b434
Aleksei <supportalex@rambler.ru>
2020-02-26 22:40:53 +0100

    full

---
+++ HW2.9/ViewControllers/MainViewController.swift
@@ -0,0 +1,73 @@
+//
+//  MainViewController.swift
+//  HW2.9
+//
+//  Created by Алексей Маслобоев on 26.02.2020.
+//  Copyright © 2020 Алексей Маслобоев. All rights reserved.
+//
+
+import UIKit
+import Spring
+
+class MainViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var nameAnimationLabal: UILabel!
+    @IBOutlet weak var animationView: SpringView!
+    @IBOutlet weak var startAnimationButton: UIButton!
+    
+    // MARK: - Private Properties
+    
+    private let animations = Spring.AnimationPreset.allCases
+    private var currentAnimation: Spring.AnimationPreset!
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        setupUI()
+        currentAnimation = animations.first
+        startAnimationAction()
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func startAnimationAction() {
+        startAnimation(currentAnimation.rawValue)
+        prepareNextAnimation()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupUI() {
+        animationView.layer.cornerRadius = 15
+        startAnimationButton.layer.cornerRadius = 15
+    }
+    
+    private func startAnimation(_ animation: String) {
+        nameAnimationLabal.text = animation
+        animationView.animation = animation
+        animationView.animate()
+        animationView.animateToNext {
+            self.animationView.animation = Spring.AnimationPreset.FadeIn.rawValue
+            self.animationView.animate()
+        }
+    }
+    
+    private func prepareNextAnimation() {
+        currentAnimation = animations.next(item: currentAnimation)
+        startAnimationButton.setTitle(currentAnimation.rawValue, for: .normal)
+    }
+
+}
+
+extension Array where Element: Hashable {
+    func next(item: Element) -> Element? {
+        if let index = self.firstIndex(of: item), index + 1 <= self.count {
+            return index + 1 == self.count ? self[0] : self[index + 1]
+        }
+        return nil
+    }
+}
